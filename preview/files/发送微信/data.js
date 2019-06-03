8ddd0044fa44221bdcdd83d0273a56730042453b
tata <jochy111@qq.com>
2019-06-03 13:56:32 +0800

    +1 folder

---
+++ preview/files/发送微信/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w],x,_(y,z,A,B,C,D,E,_(),F,_(G,H,I,J,K,_(L,M,N,O),P,null,Q,R,S,J,T,U,V,null,W,X,Y,Z,ba,bb,bc,X),bd,_(),be,_(),bf,_(bg,[])),bh,_(),bi,_());}; 
+var b="url",c="发送微信.html",d="generationDate",e=new Date(1559541184740.1),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="PercCalcValue",o="PercIncreaseNum",p="sfz",q="sfz_n",r="L",s="R",t="Lhelp",u="Rhelp",v="Ldisplay",w="Rdisplay",x="page",y="packageId",z="c660ed73c3914879b81058810f230a0f",A="type",B="Axure:Page",C="name",D="发送微信",E="notes",F="style",G="baseStyle",H="627587b6038d43cca051c114ac41ad32",I="pageAlignment",J="center",K="fill",L="fillType",M="solid",N="color",O=0xFFF2F2F2,P="image",Q="imageHorizontalAlignment",R="near",S="imageVerticalAlignment",T="imageRepeat",U="auto",V="favicon",W="sketchFactor",X="0",Y="colorStyle",Z="appliedColor",ba="fontName",bb="Applied Font",bc="borderWidth",bd="adaptiveStyles",be="interactionMap",bf="diagram",bg="objects",bh="masters",bi="objectPaths";
+return _creator();
+})());
